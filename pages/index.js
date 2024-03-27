f92bd71669a36481fc87835d46bf174052f51e1a
Joabe Diniz <130418085+jwdiniz2@users.noreply.github.com>
2024-03-27 20:40:32 +0000

    Alteração da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Não procure amigos, preserve os que a vida lhe dá.</h1>
+    return <h1>Podemos já vislumbrar o modo pelo qual o fenômeno da Internet nos obriga à análise do fluxo de informações...</h1>
 }
 
 export default Home;
