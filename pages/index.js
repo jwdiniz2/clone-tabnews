d4c81e98066c99362eea56b951d4c978eb435456
Joabe Diniz <130418085+jwdiniz2@users.noreply.github.com>
2024-03-27 20:29:33 +0000

    Alteração da frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home(){
-    return <h1>Um elogio no momento certo pode fazer milagres no dia e na vida de quem o recebe!.</h1>
+    return <h1>Não procure amigos, preserve os que a vida lhe dá.</h1>
 }
 
 export default Home;
+
 
